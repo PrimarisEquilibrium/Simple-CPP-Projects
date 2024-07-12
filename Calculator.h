b3c63e64c2ba5031e7a9f46d7ce121a1d67eb4a2
Kevin <110409671+PrimarisEquilibrium@users.noreply.github.com>
2024-07-12 14:19:58 -0400

    Moved the calculator loop to Calculator.h to make room for more simple projects

--- Calculator.h
+++ Calculator.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <iostream>
+#include "Utils.h"
 
 // Performs the passed operation from the two given numbers
 double calculator(double n1, double n2, char operation)
@@ -27,4 +28,51 @@ double calculator(double n1, double n2, char operation)
 			throw "Invalid operation, the only valid operations are (+, -, *, /)";
 			break;
 		}
+}
+
+double n1, n2;
+char operation;
+char validOperations[4] = { '+', '-', '*', '/' };
+std::string divider = "------------------------------- \n";
+
+// Calculator loop state variable
+bool selectingOperation = true;
+
+int calculatorLoop(bool& running) // Get the reference to the running variable in memory
+{
+	while (running) {
+		std::cout << "Type the first number: ";
+		std::cin >> n1;
+		std::cout << "Type the second number: ";
+		std::cin >> n2;
+
+		// Loop operation selection until a valid choice is made
+		while (selectingOperation) {
+			std::cout << "Select an operation (+, -, *, /): ";
+			std::cin >> operation;
+
+			if (contains(operation, validOperations)) {
+				std::cout << "Result: " << calculator(n1, n2, operation) << "\n";
+				std::cout << "\n" << divider << "\n";
+
+				// After the result is shown give the user an option to continue using the calculator
+				bool continueResult;
+				std::cout << "Would you like to continue? (0 = false, 1 = true): ";
+				std::cin >> continueResult;
+				if (!continueResult) {
+					running = false;
+				}
+
+				selectingOperation = false; // End the loop once the valid choice is made
+			}
+			else {
+				std::cout << "Enter a valid operation \n";
+			}
+		}
+
+		// Ensures that the operation selection state occurs on the next calculation
+		selectingOperation = true;
+	}
+
+	return 0;
 }
--- main.cpp
+++ main.cpp
@@ -1,52 +1,12 @@
 #include <iostream>
 #include <string>
 #include "Calculator.h"
-#include "Utils.h"
 
-double n1, n2;
-char operation;
-char validOperations[4] = { '+', '-', '*', '/' };
-std::string divider = "------------------------------- \n";
-
-// Calculator state variables
 bool running = true;
-bool selectingOperation = true;
 
 int main()
 {	
 	while (running) {
-		std::cout << "Type the first number: ";
-		std::cin >> n1;
-		std::cout << "Type the second number: ";
-		std::cin >> n2;
-
-		// Loop operation selection until a valid choice is made
-		while (selectingOperation) {
-			std::cout << "Select an operation (+, -, *, /): ";
-			std::cin >> operation;
-
-			if (contains(operation, validOperations)) {
-				std::cout << "Result: " << calculator(n1, n2, operation) << "\n";
-				std::cout << "\n" << divider << "\n";
-
-				// After the result is shown give the user an option to continue using the calculator
-				bool continueResult;
-				std::cout << "Would you like to continue? (0 = false, 1 = true): ";
-				std::cin >> continueResult;
-				if (!continueResult) {
-					running = false;
-				}
-
-				selectingOperation = false; // End the loop once the valid choice is made
-			}
-			else {
-				std::cout << "Enter a valid operation \n";
-			}
-		}
-
-		// Ensures that the operation selection state occurs on the next calculation
-		selectingOperation = true;
+		calculatorLoop(running);
 	}
-
-    return 0;
 }
