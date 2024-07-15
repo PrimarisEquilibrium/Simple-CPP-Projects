a9b3c27007cb84c1dd8e7bf1ac29386c9543dce1
Kevin <110409671+PrimarisEquilibrium@users.noreply.github.com>
2024-07-15 09:15:39 -0400

    Added a case to add entries to the todo list

--- Todo.h
+++ Todo.h
@@ -3,38 +3,51 @@
 #include <string>
 #include <vector>
 
+// Type alias of a string vector
+typedef std::vector<std::string> strVector;
+
 // Holds all the user entries on their todo list
 // A vector is used for its dynamic size (users may add an arbitrary amount of values)
-std::vector<std::string> todoEntries;
+strVector todoEntries;
 
-static void displayEntries(std::vector<std::string> entries) {
+static void displayEntries(strVector entries) {
 	for (int i = 0; i < entries.size(); i++) {
 		std::cout << i + 1 << ". " << entries[i];
 	}
 	std::cout << "\n";
 }
 
-void todoLoop(bool& running) {
-	todoEntries.push_back("Wash the dishes \n");
-	todoEntries.push_back("Brush my teeth \n");
+void addEntry(strVector& entries) {
+	std::string newEntry{};
+	std::cout << "Enter TODO entry: ";
 
-	int userInput;
+	// Ensures that the entire string is extracted and assigned to newEntry (doesn't stop extracting at whitespace)
+	std::cin.ignore();
+	std::getline(std::cin, newEntry);
+
+	entries.push_back(newEntry);
+}
+
+void todoLoop(bool& running) {
+	int userInput{};
 	std::cout << "Enter: \n 1. View TODO list \n 2. Add TODO list entry \n 3. Delete TODO list entry \n 4. Save to .txt file \n 9. Exit program \n";
 	std::cout << "Select an option: ";
 	std::cin >> userInput;
-	std::cout << "\n";
+
 	switch (userInput) {
-	case 1:
-		// Display all entries in the todo list
-		displayEntries(todoEntries);
-		break;
-	case 2:
-		break;
-	case 3:
-		break;
-	case 4:
-		break;
-	case 9:
-		running = false;
+		case 1:
+			// Display all entries in the todo list
+			displayEntries(todoEntries);
+			break;
+		case 2:
+			// Adds an entry to the todo list
+			addEntry(todoEntries);
+			break;
+		case 3:
+			break;
+		case 4:
+			break;
+		case 9:
+			running = false;
 	}
 }
